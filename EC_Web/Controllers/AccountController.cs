869f9c57b38fbc0748bbfcbf1ee98ceb2cc2480d
tarik59 <74472702+tarik59@users.noreply.github.com>
2022-01-24 23:26:04 +0100

    Testing Mapster

--- EC_Web/Controllers/AccountController.cs
+++ EC_Web/Controllers/AccountController.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EC_Repository;
+using Mapster;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EC_Web.Controllers
 {
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string UserName { get; set; }
+    }
     public class AccountController : BaseApiController
     {
+        public readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult<UserDto> TestMapster(int id)
+        {
+            return  _db.Users.Find(id).Adapt<UserDto>();
+        }
 
     }
 }
